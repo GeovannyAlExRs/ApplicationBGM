12f8e62313ede4255c03a24a5bbb0a6aeb4e56ae
Geovanny AlEx Rs <axel94bsc@gmail.com>
2022-02-25 23:31:13 -0500

    Agregar marker user and bus y crear 3 puntos de ruta

--- app/src/main/java/com/ec/bgm/movil/application/fragments/MapsFragment.java
+++ app/src/main/java/com/ec/bgm/movil/application/fragments/MapsFragment.java
@@ -22,10 +22,16 @@ import android.widget.Toast;
 import com.ec.bgm.movil.application.R;
 import com.ec.bgm.movil.application.activities.MainActivity;
 import com.ec.bgm.movil.application.activities.SessionModeActivity;
+import com.ec.bgm.movil.application.model.Assignes_Bus;
+import com.ec.bgm.movil.application.providers.AssigneBusProvider;
 import com.ec.bgm.movil.application.providers.AuthFirebaseProvider;
+import com.ec.bgm.movil.application.providers.CodeQRProvider;
 import com.ec.bgm.movil.application.providers.GeoFirestoreProvider;
 import com.ec.bgm.movil.application.providers.PermissionsRequestor;
 import com.ec.bgm.movil.application.providers.PlatformPositioningProvider;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.GeoPoint;
 import com.here.sdk.core.Anchor2D;
 import com.here.sdk.core.Color;
 import com.here.sdk.core.GeoCoordinates;
@@ -47,6 +53,9 @@ import com.here.sdk.routing.RoutingEngine;
 import com.here.sdk.routing.RoutingError;
 import com.here.sdk.routing.Waypoint;
 
+import org.imperiumlabs.geofirestore.listeners.GeoQueryDataEventListener;
+import org.imperiumlabs.geofirestore.listeners.GeoQueryEventListener;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -65,12 +74,25 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
 
     PlatformPositioningProvider platformPositioningProvider;
 
+    SharedPreferences sharedPreferencesCode; // guardar el valor del code qr
+    private static final String CODEQR = "code";
+
     SharedPreferences sharedPreferencesUser; // identificar el tipo de usuario
     private static final String TYPE_USER = "typeUser";
 
+    SharedPreferences sharedPreferencesConection;
+    SharedPreferences.Editor editorConection;
+    private static final String BTN_CONECTION = "conection";
+
     MapMarker mapMarker;
+    MapMarker mapMarkerUser;
     private boolean isConnect = false;
     GeoCoordinates geoCoordinatesCurrent;
+
+    CodeQRProvider codeQRProvider;
+    AssigneBusProvider assigneBusProvider;
+    private String idDriverBus;
+    private boolean isFirstTime = true;
 
     GeoFirestoreProvider geoFirestoreProvider;
 
@@ -80,6 +102,7 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
     MapPolyline mapPolyline;
 
     Waypoint waypointStart;
+    Waypoint waypointMedium;
     Waypoint waypointDestination;
 
     public MapsFragment() {
@@ -95,8 +118,24 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
 
         getViewId(view, savedInstanceState);
 
+        codeQRProvider = new CodeQRProvider();
+        assigneBusProvider = new AssigneBusProvider();
+
         geoFirestoreProvider = new GeoFirestoreProvider();
         authFirebaseProvider = new AuthFirebaseProvider();
+
+        sharedPreferencesConection = MapsFragment.this.getActivity().getSharedPreferences(BTN_CONECTION, Context.MODE_PRIVATE);
+        editorConection = sharedPreferencesConection.edit();
+
+        sharedPreferencesCode = MapsFragment.this.getActivity().getSharedPreferences(CODEQR, Context.MODE_PRIVATE);
+        String idCodeQR = sharedPreferencesCode.getString("qr", "");
+
+        //OJO VERIFICAR
+        Log.d("ENTRO", "INICIO DEL MODULO - ID DEL CODE QR " + idCodeQR);
+        if (!idCodeQR.isEmpty()) {
+            Log.d("ENTRO", "ENTRO A LA CONDICION " + idCodeQR);
+            findDataQR(idCodeQR);
+        }
 
         //loadMapScene();
 
@@ -136,20 +175,25 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
         String rutaC= "RUTA - C";
         String rutaF= "RUTA - F";
 
-        double latitudeStart = -2.2143026;
-        double longitudeStart = -80.8670098;
+        double latitudeStart = -2.2143026; //Lat Santa Elena
+        double longitudeStart = -80.8670098; //Long Santa Elena
 
-        double latitudeDestination = -2.402871;
-        double longitudeDestination = -80.680492;
+        double latitudeMedium = -2.405019; //Lat Santa Elena
+        double longitudeMedium = -80.693607; //Long Santa Elena
+
+        double latitudeDestination = -2.402871; //Lat Chanduy
+        double longitudeDestination = -80.680492; // Long Chanduy
 
 
-        GeoCoordinates startGeoCoordinates = new GeoCoordinates(latitudeStart,longitudeStart); //Cambiar lat, long para cada ruta
-        GeoCoordinates destinationGeoCoordinates = new GeoCoordinates(latitudeDestination,longitudeDestination);//Cambiar lat, long para cada ruta
+        GeoCoordinates startGeoCoordinates = new GeoCoordinates(latitudeStart,longitudeStart);
+        GeoCoordinates mediumGeoCoordinates = new GeoCoordinates(latitudeMedium,longitudeMedium);
+        GeoCoordinates destinationGeoCoordinates = new GeoCoordinates(latitudeDestination,longitudeDestination);
 
         waypointStart =  new Waypoint(startGeoCoordinates);
+        waypointMedium = new Waypoint(mediumGeoCoordinates);
         waypointDestination = new Waypoint(destinationGeoCoordinates);
 
-        waypoints = new ArrayList<>(Arrays.asList(waypointStart,waypointDestination));
+        waypoints = new ArrayList<>(Arrays.asList(waypointStart,waypointMedium,waypointDestination));
 
         routingEngine.calculateRoute(waypoints,
                 new CarOptions(),
@@ -222,27 +266,108 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
 
     private void addMarker(GeoCoordinates geoCoordinates) {
         try {
-            Log.d("HERE MAPS", "Crea Marker en el mapa Coordenadas Long[" + geoCoordinates.latitude + "], Lat[" + geoCoordinates.longitude +"], alt[" + geoCoordinates.altitude +"]");
-            if (mapMarker != null) {
-                mapView.getMapScene().removeMapMarker(mapMarker);
+            sharedPreferencesUser = MapsFragment.this.getActivity().getSharedPreferences(TYPE_USER, Context.MODE_PRIVATE);
+            String selectUser = sharedPreferencesUser.getString("user", "");
+
+            if (selectUser.equals("empleado")) {
+                Log.d("HERE MAPS", "(User Empleado) Crea Marker en el mapa Coordenadas Long[" + geoCoordinates.latitude + "], Lat[" + geoCoordinates.longitude +"], alt[" + geoCoordinates.altitude +"]");
+                if (mapMarker != null) {
+                    mapView.getMapScene().removeMapMarker(mapMarker);
+                }
+
+                MapImage mapImage = MapImageFactory.fromResource(this.getResources(), R.drawable.bgm_marker);
+                Anchor2D anchor2D = new Anchor2D(0.5F, 1);
+                geoCoordinatesCurrent = new GeoCoordinates(geoCoordinates.latitude, geoCoordinates.longitude);
+                mapMarker = new MapMarker(geoCoordinatesCurrent, mapImage, anchor2D);
+                mapView.getMapScene().addMapMarker(mapMarker);
+                String idDriverUser = authFirebaseProvider.getUidFirebase();
+                locationsBus(idDriverUser);
+            }
+            if (selectUser.equals("invitado")) {
+                Log.d("HERE MAPS", "(User Invitado) Crea Marker en el mapa Coordenadas Long[" + geoCoordinates.latitude + "], Lat[" + geoCoordinates.longitude +"], alt[" + geoCoordinates.altitude +"]");
+
+                if (mapMarkerUser != null) {
+                    mapView.getMapScene().removeMapMarker(mapMarkerUser);
+                }
+
+                MapImage mapImage = MapImageFactory.fromResource(this.getResources(), R.drawable.bgm_marker_user);
+                Anchor2D anchor2D = new Anchor2D(0.5F, 1);
+                GeoCoordinates geoCoordinatesUser = new GeoCoordinates(geoCoordinates.latitude, geoCoordinates.longitude);
+                mapMarkerUser = new MapMarker(geoCoordinatesUser, mapImage, anchor2D);
+                mapView.getMapScene().addMapMarker(mapMarkerUser);
+
+                if (isFirstTime) {
+                    Log.d("HERE MAPS", "(User Invitado) entro a la bandera");
+                    isFirstTime = false;
+                    getLocationsBus(geoCoordinatesUser);
+                }
             }
 
-            MapImage mapImage = MapImageFactory.fromResource(this.getResources(), R.drawable.bgm_marker);
-            Anchor2D anchor2D = new Anchor2D(0.5F, 1);
-            geoCoordinatesCurrent = new GeoCoordinates(geoCoordinates.latitude, geoCoordinates.longitude);
-            mapMarker = new MapMarker(geoCoordinatesCurrent, mapImage, anchor2D);
-            mapView.getMapScene().addMapMarker(mapMarker);
-
-            locationsBus();
         } catch (Exception e) {
             Log.d("ENTRO", "Error Fatal: " + e);
         }
     }
 
-    private void locationsBus() {
+    private void locationsBus(String idDriverUser) {
         if (authFirebaseProvider.getUserSession()!=null) {
-            geoFirestoreProvider.saveLocations(authFirebaseProvider.getUidFirebase(), geoCoordinatesCurrent);
+            geoFirestoreProvider.saveLocations(idDriverUser, geoCoordinatesCurrent);
         }
+    }
+
+    private void getLocationsBus(GeoCoordinates geoCoordinates) {
+        geoFirestoreProvider.getLocationsBus(geoCoordinates).addGeoQueryDataEventListener(new GeoQueryDataEventListener() {
+            @Override
+            public void onDocumentEntered(DocumentSnapshot documentSnapshot, GeoPoint geoPoint) {
+                if (documentSnapshot.exists()) {
+                    String idUser = documentSnapshot.getId();
+                    Log.d("HERE MAPS", "(LocationsBus) ENTRO AL METODO getLocationsBus() Id Documento: "+ idUser + " ID USER SCANNER: " + idDriverBus);
+                    if (idUser.equals(idDriverBus)) {
+                        Log.d("HERE MAPS", "(LocationsBus) Existe el Id Documento: "+ idUser);
+                        MapImage mapImage = MapImageFactory.fromResource(MapsFragment.this.getResources(), R.drawable.bgm_marker);
+                        Anchor2D anchor2D = new Anchor2D(0.5F, 1);
+                        mapMarker = new MapMarker(new GeoCoordinates(geoPoint.getLatitude(), geoPoint.getLongitude()), mapImage, anchor2D);
+                        mapView.getMapScene().addMapMarker(mapMarker);
+                    }
+                }
+            }
+
+            @Override
+            public void onDocumentExited(DocumentSnapshot documentSnapshot) {
+                //Elimina localizacion del marcador existente
+                if (documentSnapshot.exists()) {
+                    String idUser = documentSnapshot.getId();
+                    if (idUser.equals(idDriverBus)) {
+                        mapView.getMapScene().removeMapMarker(mapMarker);
+                    }
+                }
+            }
+
+            @Override
+            public void onDocumentMoved(DocumentSnapshot documentSnapshot, GeoPoint geoPoint) {
+
+                if (documentSnapshot.exists()) {
+                    String idUser = documentSnapshot.getId();
+                    if (idUser.equals(idDriverBus)) {
+                        mapMarker.setCoordinates(new GeoCoordinates(geoPoint.getLatitude(), geoPoint.getLongitude()));
+                    }
+                }
+            }
+
+            @Override
+            public void onDocumentChanged(DocumentSnapshot documentSnapshot, GeoPoint geoPoint) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+
+            }
+
+            @Override
+            public void onGeoQueryError(Exception e) {
+
+            }
+        });
     }
 
     private void loadMapScene() {
@@ -311,6 +436,35 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
         //platformPositioningProvider.stopLocating();
     }
 
+    private void findDataQR(String idQR) {
+        codeQRProvider.readCodeQRByID(idQR).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    if (documentSnapshot.contains("gqr_asb_bus_id")) {
+                        String idAssigneBus = documentSnapshot.getString("gqr_asb_bus_id");
+                        findDataAssigneBus(idAssigneBus);
+                    }
+
+                }
+            }
+        });
+    }
+
+    // Buscar Documento ASSIGNE BUS por el ID del Documento
+    private void findDataAssigneBus(String id) {
+        Log.d("ENTRO", "ID ASSIGNE BUS " + id);
+        assigneBusProvider.getAsigneBus(id).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    Assignes_Bus assbus = documentSnapshot.toObject(Assignes_Bus.class);
+                    idDriverBus = assbus.getAsb_accompanist_id();
+                }
+            }
+        });
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
@@ -322,14 +476,16 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
                 }
                 break;
             case R.id.id_action_logout:
-                authFirebaseProvider.logout();
                 disconnect();
+                authFirebaseProvider.logout();
                 goToView(SessionModeActivity.class, true);
                 break;
         }
     }
 
     private void disconnect() {
+        editorConection.putString("action", "Conectar");
+        editorConection.apply();
         ac_btn_maps.setText("Conectar");
         isConnect = false;
         platformPositioningProvider.stopLocating();
@@ -362,9 +518,11 @@ public class MapsFragment extends Fragment implements View.OnClickListener {
         sharedPreferencesUser = MapsFragment.this.getActivity().getSharedPreferences(TYPE_USER, Context.MODE_PRIVATE);
         String selectUser = sharedPreferencesUser.getString("user", "");
 
-        if (selectUser == "empleado") {
+        if (selectUser.equals("empleado")) {
             Toast.makeText(MapsFragment.this.getActivity(), "Tipo de usuario " + selectUser, Toast.LENGTH_SHORT).show();
-        } else {
+            ac_btn_maps.setVisibility(view.VISIBLE);
+        }
+        if (selectUser.equals("invitado")) {
             Toast.makeText(MapsFragment.this.getActivity(), "Tipo de usuario " + selectUser, Toast.LENGTH_SHORT).show();
             ac_btn_maps.setVisibility(view.GONE);
             startlocationMaps();
--- app/src/main/java/com/ec/bgm/movil/application/providers/GeoFirestoreProvider.java
+++ app/src/main/java/com/ec/bgm/movil/application/providers/GeoFirestoreProvider.java
@@ -1,11 +1,15 @@
 package com.ec.bgm.movil.application.providers;
 
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.here.sdk.core.GeoCoordinates;
 
 import org.imperiumlabs.geofirestore.GeoFirestore;
+import org.imperiumlabs.geofirestore.GeoQuery;
+import org.imperiumlabs.geofirestore.listeners.EventListenerBridge;
+import org.imperiumlabs.geofirestore.listeners.GeoQueryDataEventListener;
 
 public class GeoFirestoreProvider {
 
@@ -24,5 +28,11 @@ public class GeoFirestoreProvider {
 
     public void removeLocations(String idDriver) {
         geoFirestore.removeLocation(idDriver);
+    }
+
+    public GeoQuery getLocationsBus(GeoCoordinates geoCoordinates) {
+        GeoQuery geoQuery = geoFirestore.queryAtLocation(new GeoPoint(geoCoordinates.latitude, geoCoordinates.longitude), 75);
+        geoQuery.removeAllListeners();
+        return geoQuery;
     }
 }
